c9fced379925d7237e89397fad8b50f4b9e75389
hamlet <bryan.rr@hotmail.com>
2019-06-07 11:44:41 -0500

    primer commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParser from 'body-parser';
+import cors from 'cors'
+const server = new Server();
+
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+server.app.use(cors({ origin: true, credentials: true }));
+
+server.app.use('/', router)
+server.start(() => {
+    console.log(`servidor  corriendo en el port ${server.port}`);
+}); 
---
+++ readme.md
@@ -0,0 +1,11 @@
+#sockets server
+
+reconstruir modulos de node 
+npm install
+
+generar dist 
+
+tsc -w
+
+levabntar servidor
+nodemon dist/
