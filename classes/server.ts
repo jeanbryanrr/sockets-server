bc84619baafa344d4487c48e263a34907cd067c9
bryan <bryan.rr@hotmail.com>
2019-06-08 14:25:10 -0500

    =go

--- classes/server.ts
+++ classes/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environments';
 import socketIO from 'socket.io'
 import http from 'http'
+import * as socket from '../sockets/sockets'
 export default class Server {
     private static _instance: Server;
     public app: express.Application;
@@ -24,10 +25,14 @@ export default class Server {
         this.io.on('connection', cliente => {
 
             console.log('nuevo cliente conectado');
+            socket.mensaje(cliente,this.io);
+            socket.desconectar(cliente);
         });
+
+
     }
 
     start(callback: any) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
